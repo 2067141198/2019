4b770032b2d61b44dfd121a3fce0c55a92515231
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-04-08 00:39:14 +0800

    test

--- homework/homework10_4_7/homework10_4_7/homework10_4_7.cpp
+++ homework/homework10_4_7/homework10_4_7/homework10_4_7.cpp
@@ -3,11 +3,16 @@
 #include <stdlib.h>
 
 void Print(int n){
-	for (int i = 0; i < n; i++){
+	for (int i = 1; i <= n; i++){
 		for (int j = 0; j < n - i; j++){
 			putchar(' ');
 		}
-		printf("*");
+		for (int j = 1; j <= i / 2; j++){
+			printf("%d ", 2 ^ j - 1);
+		}
+		for (int j = i / 2; j > 0; j--){
+			printf("%d ", 2 ^ j - 1);
+		}
 		printf("\n");
 	}
 }
