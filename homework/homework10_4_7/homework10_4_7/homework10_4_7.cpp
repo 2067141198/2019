504bfcc9aa05cf3859bbe274795682182164d10d
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-04-08 22:20:16 +0800

    Update homework10_4_7.cpp

--- homework/homework10_4_7/homework10_4_7/homework10_4_7.cpp
+++ homework/homework10_4_7/homework10_4_7/homework10_4_7.cpp
@@ -3,15 +3,19 @@
 #include <stdlib.h>
 
 void Print(int n){
-	for (int i = 1; i <= n; i++){
-		for (int j = 0; j < n - i; j++){
+	int arr[20][20] = { 0 };
+	int i = 0;
+	int j = 0;
+	for (i = 1; i <= n; i++){
+		for (j = 0; j < n - i; j++){
 			putchar(' ');
 		}
-		for (int j = 1; j <= i / 2; j++){
-			printf("%d ", 2 ^ j - 1);
-		}
-		for (int j = i / 2; j > 0; j--){
-			printf("%d ", 2 ^ j - 1);
+		for (j = 1; j <= i; j++){
+			arr[0][0] = 1;
+			arr[1][1] = 1;
+			arr[2][1] = 1;
+			arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+			printf("%2d",arr[i][j]);
 		}
 		printf("\n");
 	}
