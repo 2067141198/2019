83bfbe11dc23c86b9c4d7d24685124d51d33d89f
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-04-16 23:57:02 +0800

    change

--- homework/homework10_04_13/homework_04_13/test1.c
+++ homework/homework10_04_13/homework_04_13/test1.c
@@ -11,22 +11,45 @@
 //	// 返回 1的位数 
 //}
 
-
-int count_one_bits(unsigned int x){
+// 方法一
+int count_one_bits(unsigned int value){
 	int i = 0;
-	while (x > 0){
-		if ((x & 1) != 0)
+	while (value > 0){
+		if ((value & 1) != 0)
 			++i;
-		x /= 2;
+		value /= 2;
 	}
 	return i;
+}
+
+//方法二
+int count_one_bits2(unsigned int value){
+	int count = 0;
+	for (int i = value; i; i /= 2){
+		if (i % 2 == 1)
+			count++;
+	}
+	return count;
+}
+
+//方法三
+int count_one_bits3(unsigned int value){
+	int count = 0;
+	do{
+		count++;
+	} while (value &= value - 1);//value &= value - 1 可以将该数的二进制序列中的最后的那个1与掉.所以count的值相当于与掉了几个1.
+	return count;
 }
 
 int main1(){
 	int n = 0;
 	scanf("%d", &n);
 	int ret = count_one_bits(n);
+	int ret2 = count_one_bits2(n);
+	int ret3 = count_one_bits3(n);
 	printf("%d\n", ret);
+	printf("%d\n", ret2);
+	printf("%d\n", ret3);
 	system("pause");
 	return 0;
 }
--- homework/homework10_04_13/homework_04_13/test2.c
+++ homework/homework10_04_13/homework_04_13/test2.c
@@ -6,40 +6,20 @@
 //ÓĞÎó
 
 void print(int x){
-	if (x > 1)
-		print(x / 2);
-	printf("%d", x % 2);
+	int arr[34] = { 0 };
+	for (int i = 0; i < 31; i++, x /= 2){
+		arr[i] = x % 2;
+	}
+	putchar(arr[34]);
 }
 
-int print_odd(int x){
-	int i = 0;
-	while (x > 0){
-		i |= 1 << x % 2;
-		x >>= 2;
-	}
-	return i;
-}
 
-int print_even(int x){
-	int j = 0;
-	x >>= 1;
-	while (x > 0){
-		j |= 1 << x % 2;
-		x >>= 2;
-	}
-	return j;
-}
 
 int main(){
 	int num = 0;
 	scanf("%d", &num);
 	print(num);
 	printf("\n");
-	int odd = print_odd(num);
-	int even = print_even(num);
-	print(odd);
-	printf("\n");
-	print(even);
 	system("pause");
 	return 0;
 }
