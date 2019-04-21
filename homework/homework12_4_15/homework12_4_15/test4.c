9c9fda244cae2b8e8a072dc4c0748d0958618333
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-04-21 14:25:31 +0800

    Update test4.c

--- homework/homework12_4_15/homework12_4_15/test4.c
+++ homework/homework12_4_15/homework12_4_15/test4.c
@@ -12,10 +12,41 @@
 //	i ma a tneduts
 //	i am a student
 
-int main(){
-	char ch = 'I am a student ';
+//这是一个将字符串内所有元素倒序排序的函数
+void reserve_part(char *str, int start, int end){
+	int i = 0;
+	int j = 0;
+	char tmp;
+	for (i = start, j = end; i < j; i++, j--){
+		tmp = str[i];
+		str[i] = str[j];
+		str[j] = tmp;
+	}
+}
 
+int main(){
+	char src[] = "I am a student ";
+	int i = 0;
+	int start = 0; 
+	int end;
+	//运用数组遍历将字符串里的每个元素进行遍历判断。
+	for (i = 0; src[i]; i++){
+		//然后用if语句判断字符串里所有元素买哪些位置是空格，以此分离出这句话里的每个单词
+		if (src[i] == ' '){
+			end = i - 1;
+			//调用已经编写好的换位程序现将每个单词都倒着排序
+			reserve_part(src, start, end);
+			start = i + 1;
+		}
+	}
+	//用函数将最后一个单词倒着排序
+	reserve_part(src, start, i - 1);
+	//最后将整个字符串倒着排序
+	reserve_part(src, 0, i - 1);
+	puts(src);
 
 	system("pause");
 	return 0;
 }
+
+
