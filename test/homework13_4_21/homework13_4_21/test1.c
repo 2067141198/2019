20dc5523a254c832546b2e0970b40e69fb27281a
2067141198 <48401652+2067141198@users.noreply.github.com>
2019-04-23 23:09:36 +0800

    Update test1.c

--- test/homework13_4_21/homework13_4_21/test1.c
+++ test/homework13_4_21/homework13_4_21/test1.c
@@ -1,11 +1,33 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int main(){
+	int arr[10] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int p[10] = { 0 };
+	int d[10] = { 0 };
 	int i = 0;
-
+	int j = 0;
+	int count = 0;
+	for (i = 0; i < 10; i++){
+		if (arr[i] % 2){
+			p[j] = arr[i];
+			j++;
+			count++;
+		}
+		else{
+			d[j] = arr[i];
+			j++;
+		}
+	}
+	for (i = 0; i < count; i++){
+		printf("%d ", p[i]);
+	}
+	for (i = 0; i < 10; i++){
+		printf("%d ", d[count - 1 + i]);
+	}
 
 	system("pause");
 	return 0;
